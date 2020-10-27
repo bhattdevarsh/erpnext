48d0ccbac93bc51c6c91abac6bac509653fb401e
bhattdevarsh <bhatt.devarsh17@gmail.com>
2020-10-27 16:04:14 +0530

    fix: Autofetch from Job Applicant to Employee

---
+++ erpnext/patches/v13_0/job_applicant_name_split.py
@@ -0,0 +1,11 @@
+import frappe
+
+
+def execute():
+	applicant_list = frappe.get_all("Job Applicant",fields=["name", "applicant_name", ])
+	for applicant in applicant_list:
+		splitted_name = applicant.applicant_name.split(" ")
+		frappe.db.set_value("Job Applicant", applicant.name, {
+			"first_name": splitted_name[0],
+			"last_name": splitted_name[-1]
+		}, update_modified=False)
