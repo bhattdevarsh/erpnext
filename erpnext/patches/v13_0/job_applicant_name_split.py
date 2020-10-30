522e69f4b3da6fcf69e27fa863657d02674ee66e
bhattdevarsh <bhatt.devarsh17@gmail.com>
2020-10-30 14:08:17 +0530

    fix: updated patch and test record

--- erpnext/patches/v13_0/job_applicant_name_split.py
+++ erpnext/patches/v13_0/job_applicant_name_split.py
@@ -2,6 +2,7 @@ import frappe
 
 
 def execute():
+	frappe.reload_doc("hr", "doctype", "job_applicant")
 	applicant_list = frappe.get_all("Job Applicant",fields=["name", "applicant_name"])
 	for applicant in applicant_list:
 		splitted_name = applicant.applicant_name.split(" ")
